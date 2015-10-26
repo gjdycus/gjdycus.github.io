d4bc5dd87d31f5f34e5f10a48ff9f900a1663ee9
Garrett Dycus <gjdycus@gmail.com>
2015-10-25 22:24:32 -0700

    Added formspree

--- js/script.js
+++ js/script.js
@@ -168,24 +168,6 @@ $(document).ready(function(){
 		onSuccess:function(){
 			$('.sendcontact').prop("disabled",true);
 			$('p.error').html('Your message sent.');
-			var name		= $("form[name=contactform] input[name='send[name]']").val(),
-				email		= $("form[name=contactform] input[name='send[email]']").val(),
-				message		= $("form[name=contactform] textarea[name='send[message]']").val();
-
-			$.ajax({
-				url: '/send.php',
-				type: 'POST',
-				data: {type: 'contact', name : name, email : email, message : message},
-				dataType: 'json',
-				success: function(data)
-				{
-					$('.sendcontact').prop("disabled",true);
-					$('p.error').html('Your message sent.');
-				},
-				error: function(){
-					$("#xhr").removeClass('xhr');
-				}
-			});
 		}
 	});
 
